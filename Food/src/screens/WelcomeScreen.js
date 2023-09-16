a1108d99417dc757c4c0947845be9d44aec4c316
GAEL AMBEU <ambeugael128@gmail.com>
2023-09-16 13:15:42 +0000

    short modif

--- Food/src/screens/WelcomeScreen.js
+++ Food/src/screens/WelcomeScreen.js
@@ -1,10 +1,17 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { StatusBar } from 'expo-status-bar'
 
 export default function WelcomeScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>WelcomeScreen</Text>
+      <StatusBar style='light'/>
+
+      <View style={styles.imgbox}>
+        <View style={styles.imgsmallbox}>
+          <Image src="" />
+        </View>
+      </View>      
     </View>
   )
 }
@@ -23,7 +30,27 @@ const styles = StyleSheet.create({
     },
 
     space: {
-        marginVertical: 10,
+      marginVertical: 10,
     },
+
+    imgbox: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'rgba(255, 255, 255, 0.2)', 
+      borderRadius: 999, 
+      width: 100, 
+      height: 100, 
+    },
+
+    imgsmallbox:{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'rgba(255, 255, 255, 0.2)', 
+      borderRadius: 999, 
+      width: 100, 
+      height: 100, 
+    }
   
   })
