b66d0189bfc3aed0b2b8d09ca39b5dac63b25dd1
GAEL AMBEU <ambeugael128@gmail.com>
2023-09-20 17:29:44 +0000

    short modif

--- bottom_animation/src/screens/Products.tsx
+++ bottom_animation/src/screens/Products.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Products = () => {
   return (
-    <View>
+    <View style={styles.container} >
       <Text>Products</Text>
     </View>
   )
@@ -11,4 +11,11 @@ const Products = () => {
 
 export default Products
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
