91fbe1cad391bb37487c2470677e7a95175d60ca
GAEL AMBEU <ambeugael128@gmail.com>
2023-09-20 21:26:23 +0000

    iconButton

---
+++ bottom_animation/src/components/BottomTabs/IconButton.tsx
@@ -0,0 +1,11 @@
+import React, {FC} from "react";
+import { 
+    Pressable,
+    Text,
+    StyleProp,
+    ViewStyle,
+    StyleSheet,
+    PressableProps
+} from "react-native";
+import Feather from "@expo/vector-icons/Feather";
+import MaterialCommunityIcons  from "@expo/vector-icons/MaterialCommunityIcons";
