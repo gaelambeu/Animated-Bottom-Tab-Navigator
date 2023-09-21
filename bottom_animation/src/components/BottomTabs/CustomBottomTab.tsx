a2a1341b499ce77785c108fcfcff99bd858fcd4c
GAEL AMBEU <ambeugael128@gmail.com>
2023-09-21 14:32:26 +0000

    few

--- bottom_animation/src/components/BottomTabs/CustomBottomTab.tsx
+++ bottom_animation/src/components/BottomTabs/CustomBottomTab.tsx
@@ -8,3 +8,10 @@ import Animated, {
     withTiming,
 } from "react-native-reanimated";
 import { interpolatePath } from "react-native-redash";
+
+import { SCREEN_WIDTH } from "../../constants/Screen";
+import usePath from "../../hooks/usePath";
+import { getPathXCenter } from "../../utils/Path";
+import TabItem from "./Tabitem";
+import AnimatedCircle from "./AnimatedCircle";
+import { BottomTabBarProps } from "@react-navigation/bottom-tabs";
